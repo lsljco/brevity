c9f335a62043b497a18b403531245a2fb75552f4
Larry Jenkins <ljenkins24@icloud.com>
2026-08-23 16:26:37 -0400

    Fix production PDF importer packaging

--- netlify/functions/sermon-notes-import.mjs
+++ netlify/functions/sermon-notes-import.mjs
@@ -1,5 +1,5 @@
 import mammoth from 'mammoth'
-import pdf from 'pdf-parse'
+import pdf from 'pdf-parse/lib/pdf-parse.js'
 import householdAuth from './household-auth.js'
 
 const {readSession}=householdAuth
