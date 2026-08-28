bbdb39825d46833d3a5bea3b272741a9244ec153
lsljco <89751054+lsljco@users.noreply.github.com>
2026-08-28 08:16:33 -0400

    Load phone drawer styles after app shell styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AppErrorBoundary from './AppErrorBoundary.jsx'
 import './App.css'
+import './MobileShell.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
