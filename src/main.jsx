99384f78742f6a5e1b58c6794582add77d0880d9
lsljco <89751054+lsljco@users.noreply.github.com>
2026-08-29 06:35:07 -0400

    Fix mobile drawer viewport and theme coverage

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import AppErrorBoundary from './AppErrorBoundary.jsx'
 import './App.css'
 import './MobileShell.css'
+import './ThemeCoverage.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
